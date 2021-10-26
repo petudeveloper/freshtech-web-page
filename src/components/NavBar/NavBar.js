9e536030872bcd144e93b2bac2bc685dee3b450c
petumazo <davidalvarezmazzo@gmail.com>
2021-10-26 18:22:30 -0500

    Solve navigation issue in about page.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,8 +4,13 @@ import {
   Dropdown, DropdownItem, DropdownMenu, DropdownToggle,
 } from 'reactstrap';
 import { GoGrabber } from 'react-icons/go';
-import { Link } from 'react-router-dom';
+import {
+  Link,
+  BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
 import styles from './NavBar.module.css';
+import Home from '../../pages/Home';
+import AboutUs from '../AboutUs/AboutUs';
 
 const NavBar = () => {
   const [dropdown, setDropdown] = useState(false);
@@ -13,62 +18,68 @@ const NavBar = () => {
   const openCloseDropdown = () => setDropdown(!dropdown);
 
   return (
-    <div>
-      <nav className={styles.navbar}>
-        <Link to="/">
-          <img className={styles.logo} src="./images/FRESHTECH-logo.png" alt="freshtech-logo" />
-        </Link>
-        <ul className={styles.list}>
-          <Link to="/home">
-            <li>Inicio</li>
+    <Router>
+      <div>
+        <nav className={styles.navbar}>
+          <Link to="/">
+            <img className={styles.logo} src="./images/FRESHTECH-logo.png" alt="freshtech-logo" />
           </Link>
-          <Link to="/about-us">
-            <li>Nosotros</li>
-          </Link>
-          <a href="/home#servicios-container">
-            <li>Servicios</li>
-          </a>
-          <a href="/home#lineas-container">
-            <li>Lineas</li>
-          </a>
-          <li>Contacto</li>
+          <ul className={styles.list}>
+            <Link to="/home">
+              <li>Inicio</li>
+            </Link>
+            <Link to="/about-us">
+              <li>Nosotros</li>
+            </Link>
+            <a href="/home#servicios-container">
+              <li>Servicios</li>
+            </a>
+            <a href="/home#lineas-container">
+              <li>Lineas</li>
+            </a>
+            <li>Contacto</li>
 
-        </ul>
+          </ul>
 
-        <Dropdown isOpen={dropdown} toggle={openCloseDropdown} className={styles.dropdown}>
-          <DropdownToggle className={styles.dropdownBtn}>
-            <GoGrabber />
-          </DropdownToggle>
+          <Dropdown isOpen={dropdown} toggle={openCloseDropdown} className={styles.dropdown}>
+            <DropdownToggle className={styles.dropdownBtn}>
+              <GoGrabber />
+            </DropdownToggle>
 
-          <DropdownMenu>
-            <DropdownItem className={styles.dropdownItem}>
-              <Link to="/home">
-                <span>Inicio</span>
-              </Link>
-            </DropdownItem>
-            <DropdownItem className={styles.dropdownItem}>
-              <Link to="/about-us">
-                <span>Nosotros</span>
-              </Link>
-            </DropdownItem>
-            <DropdownItem className={styles.dropdownItem}>
-              <a href="/home#servicios-container">
-                <span>Servicios</span>
-              </a>
-            </DropdownItem>
-            <DropdownItem className={styles.dropdownItem}>
-              <a href="/home#lineas-container">
-                <span>Lineas</span>
-              </a>
-            </DropdownItem>
-            <DropdownItem className={styles.dropdownItem}>
-              Contacto
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </nav>
-
-    </div>
+            <DropdownMenu>
+              <DropdownItem className={styles.dropdownItem}>
+                <Link to="/home">
+                  <span>Inicio</span>
+                </Link>
+              </DropdownItem>
+              <DropdownItem className={styles.dropdownItem}>
+                <Link to="/about-us">
+                  <span>Nosotros</span>
+                </Link>
+              </DropdownItem>
+              <DropdownItem className={styles.dropdownItem}>
+                <a href="/home#servicios-container">
+                  <span>Servicios</span>
+                </a>
+              </DropdownItem>
+              <DropdownItem className={styles.dropdownItem}>
+                <a href="/home#lineas-container">
+                  <span>Lineas</span>
+                </a>
+              </DropdownItem>
+              <DropdownItem className={styles.dropdownItem}>
+                Contacto
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </nav>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/about-us" component={AboutUs} />
+          <Redirect from="/" to="/home" />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
