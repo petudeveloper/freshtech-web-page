351d4f034934a947f4bd1d93b98a3f440cef2720
petumazo <davidalvarezmazzo@gmail.com>
2021-11-07 23:10:04 -0500

    Try doc form example 2 for netlify.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -18,25 +18,44 @@ class Form extends React.Component {
           escribiéndonos al WhatsApp,
           o escribiéndonos un correo.
         </p>
-        <form name="contact" netlify>
+        <form name="contact" method="POST" data-netlify="true">
           <p>
             <label>
-              Name
+              Your Name:
               {' '}
               <input type="text" name="name" />
             </label>
           </p>
           <p>
             <label>
-              Email
+              Your Email:
               {' '}
               <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Role:
+              {' '}
+              <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select>
+
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:
+              {' '}
+              <textarea name="message" />
             </label>
           </p>
           <p>
             <button type="submit">Send</button>
           </p>
         </form>
+
         <div>
           <a href="https://api.whatsapp.com/send?phone=573197292278">
             <BsWhatsapp className={styles.icon} />
