c123eb82cc490de6339d00211bf82d8844f1501e
petudeveloper <davidalvarezmazzo@gmail.com>
2021-12-04 16:08:02 -0500

    Update style for home page.

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -2,7 +2,7 @@ import styles from './BackgroundImage.module.css';
 
 const Background = () => (
   <div id="linea-container" className={styles.container}>
-    <img className={styles.mainImgHome} src="./images/blended-images.jpg" alt="refresh-with-technollogy" />
+    <img className={styles.mainImgHome} src="./images/blended-images2.png" alt="refresh-with-technollogy" />
   </div>
 );
 
