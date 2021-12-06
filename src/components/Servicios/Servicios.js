b8d8d3f0d55e11871d9c097b93a9a1f94ce919f1
jaar91 <jaarriagaramos@gmail.com>
2021-12-06 17:33:02 -0600

    add responsiveness to services and make the code DRY

--- src/components/Servicios/Servicios.js
+++ src/components/Servicios/Servicios.js
@@ -1,83 +1,88 @@
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import styles from './Servicios.module.css';
 
-const Servicios = () => (
-  <div id="servicios-container" className={styles.container}>
-    <h1 className={styles.textH1Home}>SERVICIOS</h1>
-    <ul className={styles.cardsContainer}>
-      <li id="asesorias-Card" className={styles.cardContainer}>
-        <img className={styles.image} src="./images/servicios/Asesorias.jpg" alt="asesoriasCard" />
-        <h3 className={styles.cardName}>
-          Asesorias
-        </h3>
-        <ul className={styles.information}>
-          <li>1. Proyectos de instalación de sistemas de aire acondicionado.</li>
-          <li>2. Mantenimientos preventivos y correctivos.</li>
-        </ul>
-        <div className={styles.buttonContainer}>
-          <button type="button" className={styles.button}>
-            <Link to="/Cotizar" className={styles.buttonSpan}>Cotizar</Link>
-          </button>
-        </div>
-      </li>
-      <li id="diseño-Card" className={styles.cardContainer}>
-        <img className={styles.image} src="./images/servicios/Diseño.jpg" alt="diseñoCard" />
-        <h3 className={styles.cardName}>
-          Diseño
-        </h3>
-        <ul className={styles.information}>
-          <li>1. Sistemas VRF.</li>
-          <li>
-            2. Sistemas aire acondicionado de expansión directa tipo Split, cassette,
-            piso techo, centrales, fancoil.
-          </li>
-          <li>3. Sistemas aires acondicionados hidrónicos.</li>
-          <li>4. Sistemas de ventilación y extracción.</li>
-        </ul>
-        <div className={styles.buttonContainer}>
-          <button type="button" className={styles.button}>
-            <Link to="/cotizar" className={styles.buttonSpan}>Cotizar</Link>
-          </button>
-        </div>
-      </li>
-      <li id="Instalacion-Card" className={styles.cardContainer}>
-        <img className={styles.image} src="./images/servicios/Instalacion.jpg" alt="instalacionCard" />
-        <h3 className={styles.cardName}>
-          Instalación
-        </h3>
-        <ul className={styles.information}>
-          <li>1. Sistemas VRF.</li>
-          <li>
-            2. Sistemas aire acondicionado de expansión directa tipo Split, cassette,
-            piso techo, centrales, fancoil, multi v.
-          </li>
-          <li>3. Sistemas aires acondicionados hidrónicos.</li>
-          <li>4. Sistemas de ventilación y extracción.</li>
-        </ul>
-        <div className={styles.buttonContainer}>
-          <button type="button" className={styles.button}>
-            <Link to="/Cotizar" className={styles.buttonSpan}>Cotizar</Link>
-          </button>
-        </div>
-      </li>
-      <li id="Mantenimiento-Card" className={styles.cardContainer}>
-        <img className={styles.image} src="./images/servicios/Mantenimiento.jpg" alt="mantenimientoCard" />
-        <h3 className={styles.cardName}>
-          Mantenimiento
-        </h3>
-        <ul className={styles.information}>
-          <li>1. Mantenimiento preventivo.</li>
-          <li>2. Mantenimiento correctivo.</li>
-          <li>3. Mantenimiento predictivo.</li>
-        </ul>
-        <div className={styles.buttonContainer}>
-          <button type="button" className={styles.button}>
-            <Link to="/Cotizar" className={styles.buttonSpan}>Cotizar</Link>
-          </button>
-        </div>
-      </li>
-    </ul>
-  </div>
-);
+const Servicios = () => {
+  const services = [
+    {
+      id: 1,
+      img: './images/servicios/Asesorias.jpg',
+      title: 'Asesorias',
+      body: [
+        '1. Proyectos de instalación de sistemas de aire acondicionado.',
+        '2. Mantenimientos preventivos y correctivos.',
+      ],
+    },
+    {
+      id: 2,
+      img: './images/servicios/Diseño.jpg',
+      title: 'Diseño',
+      body: [
+        '1. Sistemas VRF.',
+        '2. Sistemas aire acondicionado de expansión directa tipo Split, cassette, piso techo, centrales, fancoil.',
+        '3. Sistemas aires acondicionados hidrónicos.',
+        '4. Sistemas de ventilación y extracción.',
+      ],
+    },
+    {
+      id: 3,
+      img: './images/servicios/Instalacion.jpg',
+      title: 'Instalacion',
+      body: [
+        '1. Sistemas VRF.',
+        '2. Sistemas aire acondicionado de expansión directa tipo Split, cassette, piso techo, centrales, fancoil, multi v.',
+        '3. Sistemas aires acondicionados hidrónicos.',
+        '4. Sistemas de ventilación y extracción.',
+      ],
+    },
+    {
+      id: 4,
+      img: './images/servicios/Mantenimiento.jpg',
+      title: 'Mantenimiento',
+      body: [
+        '1. Mantenimiento preventivo.',
+        '2. Mantenimiento correctivo.',
+        '3. Mantenimiento predictivo.',
+      ],
+    },
+  ];
+  return (
+    <div id="servicios-container" className="w-100 p-0">
+      <h1 className={styles.textH1Home}>SERVICIOS</h1>
+      <ul className="row m-0">
+        {
+          services.map((service) => (
+            <li key={service.id} id="asesorias-Card" className="col-md-12 col-lg-6 col-xl-3 p-1 h-100">
+              <div className="position-relative rounded shadow p-2 h-100 row m-0">
+                <div className="col-12 col-sm-6 col-xl-12">
+                  <img className="w-100 rounded" src={service.img} alt="asesoriasCard" />
+                </div>
+                <div className="col-12 col-sm-6 col-xl-12 d-flex flex-column justify-content-center">
+                  <h3 className={styles.cardName}>{service.title}</h3>
+                  <ul className={styles.information}>
+                    {
+                      service.body.map((item, index) => {
+                        const key = index + 1;
+                        return (
+                          <li key={key}>
+                            {item}
+                          </li>
+                        );
+                      })
+                    }
+                  </ul>
+                  <div className="text-end">
+                    <button type="button" className="btn btn-outline-info">
+                      <Link to="/Cotizar" className="text-decoration-none">Cotizar</Link>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
 
 export default Servicios;
