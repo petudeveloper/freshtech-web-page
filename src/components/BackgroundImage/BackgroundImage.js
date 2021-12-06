de8c76de3e8aa5baa5c74693f1c5f7fa6ae8d7fa
jaar91 <jaarriagaramos@gmail.com>
2021-12-06 15:44:31 -0600

    update home page design and change background image

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -2,7 +2,13 @@ import styles from './BackgroundImage.module.css';
 
 const Background = () => (
   <div id="linea-container" className={styles.container}>
-    <img className={styles.mainImgHome} src="./images/blended-images2.png" alt="refresh-with-technollogy" />
+    <div className={styles.mainImgHome}>
+      <div className={styles.textcontainer}>
+        <p className={styles.titlea}>Especialistas en:</p>
+        <p className={styles.titleb}>Dieseño, Instalacion y Mantenimiento</p>
+        <p className={styles.titlea}>de sistemas de aire acondicionado</p>
+      </div>
+    </div>
   </div>
 );
 
