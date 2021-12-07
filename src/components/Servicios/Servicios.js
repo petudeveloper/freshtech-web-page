7f5de2dcfd0b338efa2032d9ecd000a01cde3af4
jaar91 <jaarriagaramos@gmail.com>
2021-12-07 17:03:44 -0600

    update services style and fix size issues with images

--- src/components/Servicios/Servicios.js
+++ src/components/Servicios/Servicios.js
@@ -5,15 +5,6 @@ const Servicios = () => {
   const services = [
     {
       id: 1,
-      img: './images/servicios/Asesorias.jpg',
-      title: 'Asesorias',
-      body: [
-        '1. Proyectos de instalación de sistemas de aire acondicionado.',
-        '2. Mantenimientos preventivos y correctivos.',
-      ],
-    },
-    {
-      id: 2,
       img: './images/servicios/Diseño.jpg',
       title: 'Diseño',
       body: [
@@ -24,7 +15,7 @@ const Servicios = () => {
       ],
     },
     {
-      id: 3,
+      id: 2,
       img: './images/servicios/Instalacion.jpg',
       title: 'Instalacion',
       body: [
@@ -32,6 +23,15 @@ const Servicios = () => {
         '2. Sistemas aire acondicionado de expansión directa tipo Split, cassette, piso techo, centrales, fancoil, multi v.',
         '3. Sistemas aires acondicionados hidrónicos.',
         '4. Sistemas de ventilación y extracción.',
+      ],
+    },
+    {
+      id: 3,
+      img: './images/servicios/Asesorias.jpg',
+      title: 'Asesorias',
+      body: [
+        '1. Proyectos de instalación de sistemas de aire acondicionado.',
+        '2. Mantenimientos preventivos y correctivos.',
       ],
     },
     {
@@ -48,37 +48,41 @@ const Servicios = () => {
   return (
     <div id="servicios-container" className="w-100 p-0">
       <h1 className={styles.textH1Home}>SERVICIOS</h1>
-      <ul className="row m-0">
+      <ul className="row m-0 p-0">
         {
-          services.map((service) => (
-            <li key={service.id} id="asesorias-Card" className="col-md-12 col-lg-6 col-xl-3 p-1 h-100">
-              <div className="position-relative rounded shadow p-2 h-100 row m-0">
-                <div className="col-12 col-sm-6 col-xl-12">
-                  <img className="w-100 rounded" src={service.img} alt="asesoriasCard" />
-                </div>
-                <div className="col-12 col-sm-6 col-xl-12 d-flex flex-column justify-content-center">
-                  <h3 className={styles.cardName}>{service.title}</h3>
-                  <ul className={styles.information}>
-                    {
-                      service.body.map((item, index) => {
-                        const key = index + 1;
-                        return (
-                          <li key={key}>
-                            {item}
-                          </li>
-                        );
-                      })
-                    }
-                  </ul>
-                  <div className="text-end">
-                    <button type="button" className="btn btn-outline-info">
-                      <Link to="/Cotizar" className="text-decoration-none">Cotizar</Link>
-                    </button>
+          services.map((service, index) => {
+            const photoDiv = 'col-12 col-sm-6 col-md-12 col-xl-12 p-1 ' + (([1, 0][index % 2]) === 0 ? 'order-0' : 'order-1');
+            const infoDiv = 'col-12 col-sm-6 col-md-12 col-xl-12 d-flex flex-column justify-content-around ' + (([1, 0][index % 2]) === 0 ? 'order-1' : 'order-0');
+            return (
+              <li key={service.id} id="asesorias-Card" className="col-sm-12 col-md-6 col-lg-6 col-xl-3 p-3">
+                <div className="position-relative rounded shadow p-2 h-100 row m-0">
+                  <div className={photoDiv}>
+                    <img className="w-100 rounded m-0" src={service.img} alt="asesoriasCard" />
+                  </div>
+                  <div className={infoDiv}>
+                    <h3 className={styles.cardName}>{service.title}</h3>
+                    <ul className="list-style-none">
+                      {
+                        service.body.map((item, index) => {
+                          const key = index + 1;
+                          return (
+                            <li key={key}>
+                              {item}
+                            </li>
+                          );
+                        })
+                      }
+                    </ul>
+                    <div className="text-end">
+                      <button type="button" className="btn btn-outline-info">
+                        <Link to="/Cotizar" className="text-decoration-none">Cotizar</Link>
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </li>
-          ))
+              </li>
+            );
+          })
         }
       </ul>
     </div>
